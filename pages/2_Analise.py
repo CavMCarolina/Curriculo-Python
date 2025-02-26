ad9f07f28283a0c22792bc5d39f84fda5efcb171
CavMCarolina <rm552925@fiap.com.br>
2025-02-26 11:00:51 -0300

    Começando o Curriculo e tentando entender a biblioteca :)

---
+++ pages/2_Analise.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+col1, col2 = st.columns([0.4, 0.6])
+col1.image("Foto.jpeg")
+
+col2.title("Carolina Cavalli Machado")
+col2.write("aduahsudh")
